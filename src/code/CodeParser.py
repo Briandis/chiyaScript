b4772d30c2f388dbfa3763545955fd7e7fb1d746
chiya <wei895926300@126.com>
2023-06-16 16:19:26 +0800

    解析器性能优化

--- src/code/CodeParser.py
+++ src/code/CodeParser.py
@@ -1,5 +1,94 @@
 import time
-from typing import List
+from typing import List, Dict
+
+
+class MatchRule:
+
+    def __init__(self, data: str, need_match=False, ignore_case=False):
+        self.data = data
+        """ 匹配的字符 """
+        self.need_match: bool = need_match
+        """ 需要进行匹配 """
+        self.ignore_case: bool = ignore_case
+        """ 忽略大小写 """
+        self.length: int = len(data)
+        """ 长度 """
+
+
+class IndexTree:
+    class _TreeNode:
+        def __init__(self):
+            """ 节点 """
+            self.children: Dict[str, IndexTree._TreeNode] = {}
+            """ 子节点 """
+            self.end = []
+            """ 包含 """
+            self.need_match: List[IndexTree._TreeNode] = []
+
+        def add(self, key):
+            """
+            添加索引的信息
+            :param key:索引的key
+            :return: 当前这个key的节点
+            """
+            if key not in self.children:
+                self.children[key] = IndexTree._TreeNode()
+            return self.children[key]
+
+    def __init__(self):
+        self.root: IndexTree._TreeNode = self._TreeNode()
+        """ 根节点 """
+        self.match_node = self.root
+        """ 当前匹配节点 """
+
+    def reset(self):
+        self.match_node = self.root
+
+    @staticmethod
+    def _to_match_rule(data: str, need_match=False, ignore_case=False):
+        if isinstance(data, str):
+            return MatchRule(data, need_match, ignore_case)
+        return data
+
+    @staticmethod
+    def create_index(key, start_node: _TreeNode, match_rule: MatchRule):
+        node_list = set()
+        node_list.add(start_node)
+        for now_char in match_rule.data:
+            next_node = set()
+            # 忽略大小写情况，对所有路径进行添加
+            for now_node in node_list:
+                if match_rule.ignore_case:
+                    next_node.add(now_node.add(now_char.lower()))
+                    next_node.add(now_node.add(now_char.upper()))
+                else:
+                    next_node.add(now_node.add(now_char))
+            node_list = next_node
+
+        for now_node in node_list:
+            now_node.end.append(key)
+        return node_list
+
+    def add(self, key, start_data: MatchRule | str):
+        """
+        添加
+        :param key:唯一标识
+        :param start_data: 数据
+        """
+        start_data = self._to_match_rule(start_data)
+        self.create_index(key, self.root, start_data)
+
+    def match(self, now_char):
+        """
+        迭代匹配
+        :param now_char:当前字符
+        """
+        is_match = now_char in self.match_node.children
+        result = []
+        if is_match:
+            self.match_node = self.match_node.children[now_char]
+            result = self.match_node.end
+        return result, not is_match or not self.match_node.children
 
 
 class IterativeMatch:
@@ -41,7 +130,7 @@ class IterativeMatch:
         return True, is_equal
 
 
-class Factor:
+class TokenRule:
 
     def __init__(self, status, start, end, need_escape=False, next_parser=None, self_mark=None):
         """
@@ -67,13 +156,11 @@ class Factor:
         """ 自身标记 """
 
 
-class MatchFactor:
-    def __init__(self, factor: Factor):
-        self.factor: Factor = factor
+class MatchToken:
+    def __init__(self, token_rule: TokenRule):
+        self.token_rule: TokenRule = token_rule
         """ 因子 """
-        self.start_match = IterativeMatch(self.factor.start)
-        """ 开始匹配 """
-        self.end_match = IterativeMatch(self.factor.end)
+        self.end_match = IterativeMatch(self.token_rule.end)
         """ 结尾匹配 """
         self.cache = ""
         """ 缓存中间字符 """
@@ -81,38 +168,6 @@ class MatchFactor:
         """ 转义字符状态 """
         self.end_index = -1
         """ 最终停止的字符位置 """
-        self.next_flag = False
-
-    def reset(self):
-        """ 重置 """
-        self.start_match.reset()
-        self.start_match.reset()
-        self.cache = ""
-        self.escape = False
-        self.end_index = -1
-        self.next_flag = False
-
-    def prefix(self, next_char):
-        """
-        匹配字符
-        :param next_char:下一个匹配字符
-        :return: True/False
-        """
-        if self.next_flag:
-            return self.prefix_end(next_char)
-        return self.prefix_start(next_char)
-
-    def prefix_start(self, next_char):
-        """
-        判断该字符串是不是前缀，
-        :param next_char: 下一个字符
-        :return: True/False
-        """
-        similar, equal = self.start_match.match(next_char)
-        if equal:
-            self.next_flag = True
-            equal = self.factor.end is None
-        return similar, equal
 
     def prefix_end(self, next_char):
         """
@@ -120,10 +175,10 @@ class MatchFactor:
         :param next_char: 下一个字符
         :return: True/False
         """
-        if self.factor.end is None:
+        if self.token_rule.end is None:
             return False, True
         # 转义字符
-        if self.factor.need_escape:
+        if self.token_rule.need_escape:
             if self.escape:
                 escape_char = f'\\{next_char}'
                 if escape_char == "\\\\":
@@ -143,8 +198,16 @@ class MatchFactor:
         self.cache += next_char
         similar, equal = self.end_match.match(next_char)
         if equal:
-            self.cache = self.cache[0:len(self.cache) - len(self.factor.end)]
+            self.cache = self.cache[0:len(self.cache) - len(self.token_rule.end)]
         return True, equal
+
+
+class MatchResult:
+
+    def __init__(self, token_rule: TokenRule, end_index: int, data: str = ""):
+        self.token_rule = token_rule
+        self.end_index = end_index
+        self.data = data
 
 
 class Token:
@@ -173,7 +236,7 @@ class Token:
         token.type = any_type
         return token
 
-    def __init__(self, match_factor: MatchFactor = None):
+    def __init__(self, match_factor: MatchResult = None):
         self.type = None
         """ 类型 """
         self.token_tree = []
@@ -190,10 +253,10 @@ class Token:
         """ 源文件字符位置 """
 
         if match_factor is not None:
-            self.start = match_factor.factor.start
-            self.end = match_factor.factor.end
-            self.data = match_factor.cache
-            self.type = match_factor.factor.status
+            self.start = match_factor.token_rule.start
+            self.end = match_factor.token_rule.end
+            self.data = match_factor.data
+            self.type = match_factor.token_rule.status
             self.end_index = match_factor.end_index
 
     def add_tree(self, token):
@@ -204,15 +267,17 @@ class Token:
         self.token_tree.append(token)
 
 
-class CombinationFactor:
+class ParserMatch:
 
     def __init__(self):
-        self.data: List[Factor] = []
+        self.data: List[TokenRule] = []
         """ 全部因子信息 """
-        self.cache: List[MatchFactor] = []
-        """ 匹配器缓存 """
+        self.index_tree = IndexTree()
+        """ 前缀匹配索引 """
+        self.match_index = {}
+        """ 后缀匹配器索引 """
 
-    def add_factor(self, status, start, end=None, next_parser=None, self_mark=None, need_escape=False):
+    def add_rule(self, status, start, end=None, next_parser=None, self_mark=None, need_escape=False):
         """
         添加因子
         :param status:状态
@@ -222,30 +287,47 @@ class CombinationFactor:
         :param self_mark:自身标记
         :param need_escape:需要转义处理
         """
-        self.data.append(Factor(status, start, end, need_escape, next_parser=next_parser, self_mark=self_mark))
+        self.data.append(TokenRule(status, start, end, need_escape, next_parser=next_parser, self_mark=self_mark))
+        self.index_tree.add(len(self.data) - 1, start)
+        if end:
+            self.match_index[len(self.data) - 1] = self.data[-1]
 
-    def clear_cache(self):
-        """ 清除缓存 """
-        self.cache = []
+    def match(self, index, source_code) -> List[MatchResult]:
+        self.index_tree.reset()
+        result: List[MatchResult] = []
+        judge_match: List[MatchToken] = []
+        next_match: List[MatchToken] = []
+        is_over = False
+        while index < len(source_code):
+            now_char = source_code[index]
 
-    def to_match_factor(self) -> List[MatchFactor]:
-        """
-        制造一批匹配因子
-        :return: 复制的因子列表
-        """
-        if not self.cache:
-            self.cache = []
-            for factor in self.data:
-                self.cache.append(MatchFactor(factor))
-        else:
-            for match_factor in self.cache:
-                match_factor.reset()
-        return self.cache
+            for match in judge_match:
+                similar, equal = match.prefix_end(now_char)
+                # 前缀相同则放入符合因子中
+                if equal:
+                    result.append(MatchResult(match.token_rule, index, match.cache))
+                # 相似则需要进一步判断
+                elif similar:
+                    next_match.append(match)
+            judge_match, next_match = next_match, []
+
+            if not is_over:
+                key_list, is_over = self.index_tree.match(now_char)
+                for key in key_list:
+                    if key in self.match_index:
+                        match_factor = MatchToken(self.data[key])
+                        judge_match.append(match_factor)
+                    else:
+                        result.append(MatchResult(self.data[key], index))
+            if is_over and not judge_match:
+                break
+            index += 1
+        return result
 
 
 class CodeParser:
     def __init__(self):
-        self.factor = CombinationFactor()
+        self.parser_match = ParserMatch()
         """ 因子 """
 
     def add_token(self, token_type: str, *args: str):
@@ -255,7 +337,7 @@ class CodeParser:
         :param args: token
         """
         for token in args:
-            self.factor.add_factor(token_type, token)
+            self.parser_match.add_rule(token_type, token)
 
     def add_combination(self, token_type: str, start: str, end: str, next_parser=None, self_mark=None, need_escape=False):
         """
@@ -267,34 +349,7 @@ class CodeParser:
         :param self_mark: 自身标记
         :param need_escape: 需要转义匹配
         """
-        self.factor.add_factor(token_type, start, end, next_parser, self_mark, need_escape)
-
-    def switch_token(self, index, source_code):
-        # 符合的因子
-        satisfy_factor = []
-        # 判断起始因子队列
-        judge_start_list = self.factor.to_match_factor()
-        # 下一次待判断因子
-        next_start_list = []
-        while index < len(source_code):
-            now_char = source_code[index]
-            for match in judge_start_list:
-                similar, equal = match.prefix(now_char)
-                # 前缀相同则放入符合因子中
-                if equal:
-                    match.end_index = index
-                    satisfy_factor.append(match)
-                # 相似则需要进一步判断
-                elif similar:
-                    next_start_list.append(match)
-            # 队列交换操作
-            judge_start_list, next_start_list = next_start_list, []
-            if not judge_start_list:
-                break
-            index += 1
-        # 排序，起始标识进行升序
-        satisfy_factor.sort(key=lambda x: x.factor.start)
-        return satisfy_factor
+        self.parser_match.add_rule(token_type, start, end, next_parser, self_mark, need_escape)
 
     def to_token(self, source_code, any_type="any", skip_type=None) -> List[Token]:
         """
@@ -316,39 +371,37 @@ class CodeParser:
         now_index = 0
         any_token = ""
         while now_index < len(source_code):
-            find_token = self.switch_token(now_index, source_code)
-            if len(find_token) == 0:
-                any_token += source_code[now_index]
-            else:
+            match_result = self.parser_match.match(now_index, source_code)
+            if match_result:
                 if any_token != "":
                     token_list.append(Token.any_token(any_type, any_token, now_index - 1))
                     any_token = ""
-
-                last_token = find_token[-1]
-                if last_token.factor.status not in skip_type:
+                last_match = match_result[-1]
+                if last_match.token_rule.status not in skip_type:
                     # 缓存结尾下标，防止递归解析时，信息丢失
-                    end_index = last_token.end_index
+                    end_index = last_match.end_index
                     # 如果需要递归解析
-                    if last_token.factor.next_parser is not None:
+                    if last_match.token_rule.next_parser is not None:
                         # 先进性计算，递归解析中会将当前状态信息重置
-                        start_index = end_index - len(last_token.cache) - 1
-                        self_mark = last_token.factor.status
+                        start_index = end_index - len(last_match.data) - 1
+                        self_mark = last_match.token_rule.status
                         # 如果有自身类型，则装配自身类型
-                        if last_token.factor.self_mark:
-                            self_mark = last_token.factor.self_mark
+                        if last_match.token_rule.self_mark:
+                            self_mark = last_match.token_rule.self_mark
                         # 进行递归解析
-                        temp_token_list = last_token.factor.next_parser.to_token(last_token.cache, last_token.factor.status, skip_type)
+                        temp_token_list = last_match.token_rule.next_parser.to_token(last_match.data, last_match.token_rule.status, skip_type)
                         # 此处计算的是起始坐标
-                        token_list.append(Token.start_type(self_mark, last_token.factor.start, start_index))
+                        token_list.append(Token.start_type(self_mark, last_match.token_rule.start, start_index))
                         for token in temp_token_list:
                             token.end_index = start_index + token.end_index + 1
                         token_list.extend(temp_token_list)
-                        token_list.append(Token.end_type(self_mark, last_token.factor.end, end_index))
+                        token_list.append(Token.end_type(self_mark, last_match.token_rule.end, end_index))
 
                     else:
-                        token_list.append(Token(last_token))
+                        token_list.append(Token(last_match))
                     now_index = end_index
-
+            else:
+                any_token += source_code[now_index]
             now_index += 1
         if any_token != "":
             token_list.append(Token.any_token(any_type, any_token, now_index - 1))
